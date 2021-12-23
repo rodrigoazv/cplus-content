ee06385af2a5110da6ccf5f00af68a6949afbd0b
Rodrigo Azevedo <rodrigozcma@gmail.com>
2021-12-22 22:14:29 -0300

    adding simple atoi function implements

---
+++ atoi.cpp
@@ -0,0 +1,26 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int atoi(const char *S){
+	long num = 0;
+
+	int i = 0;
+
+	while (S[i] && (S[i] >= '0' && S[i] <= '9')){
+		num = num * 10 +(S[i] - '0');
+		i++;
+	}
+
+	return num;
+
+}
+
+int main(){
+	char S[] = "123";
+
+	cout << atoi(S);
+	return 0;
+}
+
